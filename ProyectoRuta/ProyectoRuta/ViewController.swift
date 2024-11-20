d1180617c19e7a3d0de8ef9be8da68b58017995c
Angel Veliz <99308699+angelveliz1837@users.noreply.github.com>
2024-11-20 15:09:52 -0500

    Parte Basica del proyecto Movil

---
+++ ProyectoRuta/ProyectoRuta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoRuta
+//
+//  Created by DISEÑO on 20/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
